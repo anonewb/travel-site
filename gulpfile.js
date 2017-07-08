3137a9799d03717785843c9b94f45c054e891104
AnoNewb <anonewbe@gmail.com>
2017-07-08 07:15:55 +0530

    Gulp added

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars');
+nested = require('postcss-nested');
+
+
+gulp.task('default',function(){
+	console.log("yo!");
+});
+
+gulp.task('html',function(){
+	console.log("index.html file changed!!");
+});
+
+gulp.task('styles',function(){
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch',function(){
+
+	watch('./app/index.html', function(){
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**', function(){
+		gulp.start('styles');
+	});
+
+});
