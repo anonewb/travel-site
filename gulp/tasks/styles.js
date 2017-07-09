bba84361ff27be30bf259d3c44619c79754711a4
AnoNewb <anonewbe@gmail.com>
2017-07-09 14:55:18 +0530

    Gulp Error handling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,5 +9,9 @@ cssimport = require('postcss-import');
 gulp.task('styles',function(){			//styles task is responsible for any postcss to occur
 	return gulp.src('./app/assets/styles/styles.css')				//src file
 		.pipe(postcss([cssimport,cssvars, nested, autoprefixer]))	//add filters to src file
+		.on('error', function(errorInfo) {			//on refers to if. 2 parameters (name of the event, what todo)
+			console.log(errorInfo.toString());		//errorInfo contains errors
+			this.emit('end');
+		})
 		.pipe(gulp.dest('./app/temp/styles')); 						//dest file
 });
