d08f02db4d3c92a90cb369c48a8c733468174b93
AnoNewb <anonewbe@gmail.com>
2017-07-09 11:29:53 +0530

    BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');
+cssimport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 //gulp works with different tasks
 
@@ -18,20 +19,33 @@ gulp.task('html',function(){			//task() takes 2 parameters (name of task,what to
 	console.log("index.html file changed!!");
 });
 
-gulp.task('styles',function(){
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssimport,cssvars, nested, autoprefixer]))	//*****imp line
-		.pipe(gulp.dest('./app/temp/styles'));
+gulp.task('styles',function(){			//styles task is responsible for any postcss to occur
+	return gulp.src('./app/assets/styles/styles.css')				//src file
+		.pipe(postcss([cssimport,cssvars, nested, autoprefixer]))	//add filters to src file
+		.pipe(gulp.dest('./app/temp/styles')); 						//dest file
 });
 
 gulp.task('watch',function(){			//in cmd we write "gulp watch"
+ 	
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"  			//points server to baseDir(db directory) to app folder
+		}
+	});
 
 	watch('./app/index.html', function(){
-		gulp.start('html');
+		browserSync.reload();			//reloads browser asa we save ur index.html file
 	});
 
-	watch('./app/assets/styles/**', function(){
-		gulp.start('styles');
+	watch('./app/assets/styles/**', function(){	
+		gulp.start('cssInject');		//starts cssInject task but styles task will run first
+										//cssInject is very useful for JS
 	});
 
+});
+
+gulp.task('cssInject', ['styles'], function() { 	//[] consist dependencies tasks which shud be completed first then cssInject task will run
+	return	gulp.src('./app/temp/styles/styles.css') //as gulp.src() is asyncronous function we use return
+			.pipe(browserSync.stream()); 			// stream() makes available to browser whateva we are piping into it
 });
