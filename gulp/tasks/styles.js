dc65e3f999574cb2b44d73fe353ae58023b8059c
AnoNewb <anonewbe@gmail.com>
2017-07-10 12:46:02 +0530

    Mobile-First/Mixins Intro #17

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');
+cssimport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 
 gulp.task('styles',function(){			//styles task is responsible for any postcss to occur
 	return gulp.src('./app/assets/styles/styles.css')				//src file
-		.pipe(postcss([cssimport,cssvars, nested, autoprefixer]))	//add filters to src file
+		.pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))	//adds postcss filters to src file
 		.on('error', function(errorInfo) {			//on refers to if. 2 parameters (name of the event, what todo)
 			console.log(errorInfo.toString());		//errorInfo contains errors
 			this.emit('end');
