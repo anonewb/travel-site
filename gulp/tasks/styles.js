6473bc07f8428bdd1bdd6b0ca65bb2ee651d86c2
AnoNewb <anonewbe@gmail.com>
2017-07-22 17:19:25 +0530

    Part1: Styling header for large screens finished

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssimport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba'); //postcss plugin hexrgba
 
 
 gulp.task('styles',function(){			//styles task is responsible for any postcss to occur
 	return gulp.src('./app/assets/styles/styles.css')				//src file
-		.pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))	//adds postcss filters to src file
+		.pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))	//adds postcss filters to src file
 		.on('error', function(errorInfo) {			//on refers to if. 2 parameters (name of the event, what todo)
 			console.log(errorInfo.toString());		//errorInfo contains errors
 			this.emit('end');
