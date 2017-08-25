aca9c9afe8cadf414b1b5460fca425f49927d343
Bob <anonewbe@gmail.com>
2017-08-25 10:18:38 +0530

    modernizr intro

---
+++ gulp/tasks/modernizr.js
@@ -0,0 +1,14 @@
+// var gulp = require('gulp'),
+// modernizr = require('gulp-modernizr');
+
+// gulp.task('modernizr', function() {
+// 	return gulp.src('')
+// 		.pipe(modernizr())
+// 		.pipe(gulp.dest(''));
+// });
+
+// gulp.task('modernizr', function() {
+//  	return gulp.src(['',''])
+//  		.pipe(modernizr())
+//  		.pipe(gulp.dest(''));
+// });
